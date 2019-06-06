47c4caf394d9f87d35c6edae6b5f0db28ecabf77
susanshrestha28 <51448029+susanshrestha28@users.noreply.github.com>
2019-06-06 22:37:24 +0545

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     jwtSecret: 'long-live-the-ionic-academy',
-    db: 'mongodb+srv://susan:shrestha@12345@cluster0-mzivc.mongodb.net/test'
+    db: 'mongodb+srv://susan:shrestha@12345@cluster1-1wvut.mongodb.net/test'
 };
