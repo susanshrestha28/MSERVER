06ad28964d3bd323b1f2591350b6e73664b19ad8
susanshrestha28 <51448029+susanshrestha28@users.noreply.github.com>
2019-07-22 11:39:59 +0545

    a

--- controllers/GpsController.js
+++ controllers/GpsController.js
@@ -1,6 +1,7 @@
 var UserModel = require('../models/user');
 var Records = require('../models/record');
-var BusModel = require('../models/bus')
+var BusModel = require('../models/bus');
+var DatasModel = require('../models/datas'); 
 exports.getLocations = async(req, res) => {
    console.log("hello");
    try{
@@ -30,3 +31,11 @@ record.save();
         
     res.send(record)
 }
+
+exports.datas =  function(req, res) {
+    console.log("Saving datas send from aurdinogprs ....")
+          const record =new DatasModel(req.body);
+  record.save();
+          
+      res.send(record)
+  }
--- routes.js
+++ routes.js
@@ -8,5 +8,5 @@ console.log('Routes')
 routes.get('/get-locations', gpsController.getLocations)
 routes.get('/get-bus', gpsController.getBuses)
 routes.post('/add-data',gpsController.addData)
-
+routes.post('/datas',gpsController.datas)
 module.exports = routes
