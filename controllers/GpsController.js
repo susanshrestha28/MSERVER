10e890288383472085c86843203b4784bfe6ff77
susanshrestha28 <51448029+susanshrestha28@users.noreply.github.com>
2019-07-26 13:22:05 +0545

    Update GpsController.js

--- controllers/GpsController.js
+++ controllers/GpsController.js
@@ -17,7 +17,7 @@ exports.getLocations = async(req, res) => {
 exports.getBuses = async(req, res) => {
     console.log("hi");
    try{
-     const result = await DatasModel.find({}).exec()
+     const result = await BusModel.find({}).exec()
      res.send(result)
  } catch (err) {
      res.status(500).send(err);
