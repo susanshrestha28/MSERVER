9ed9e64c488c73c9b464445516d41d8ee2234114
susanshrestha28 <51448029+susanshrestha28@users.noreply.github.com>
2019-07-26 13:23:43 +0545

    Update datas.js

--- models/datas.js
+++ models/datas.js
@@ -3,9 +3,8 @@ var mongoose = require('mongoose');
 module.exports = DatasModel = mongoose.model("busdatas", {
     latitude: String,
     longitude: String,
-    time:String,
+    date:String,
     count:String,
-    Username:"Skywalkers",
-    userid:"01"
+  
 });
 
